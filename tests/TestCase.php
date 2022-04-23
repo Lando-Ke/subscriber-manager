dcf6bc9081ace5591bd64830ab11c248cb2e3c1a
Gabriel Lando <glando.py@gmail.com>
2022-04-23 11:18:14 +0300

    Make PHPUnit command relatable

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,14 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use phpDocumentor\Reflection\Types\Void_;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): Void
+    {
+        parent::setUp();
+    }
 }
