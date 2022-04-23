b23eb1e73390408534aa00b9a6b8ae1b1770580b
Gabriel Lando <glando.py@gmail.com>
2022-04-23 10:41:10 +0300

    Add config files to handle 'configurable' variables

---
+++ config/subscriber.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'fields' => [
+        'allowed' => ['date', 'number', 'string', 'boolean']
+    ],
+
+    'search_terms' => [
+        'allowed' => ['state', 'name', 'email_address']
+    ]
+];
