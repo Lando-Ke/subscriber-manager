b458bad9e6cf933ea068794635fdbd0084e96dcd
Gabriel Lando <glando.py@gmail.com>
2022-04-23 10:40:34 +0300

    Add custom exception classes to render specific errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,8 @@
 
 namespace App\Exceptions;
 
+use App\Models\State;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -12,8 +14,7 @@ class Handler extends ExceptionHandler
      *
      * @var array<int, class-string<Throwable>>
      */
-    protected $dontReport = [
-        //
+    protected $dontReport = [//
     ];
 
     /**
@@ -27,6 +28,17 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json([
+                'error' => str_replace(['App', '\\', 'Models'], '', $exception->getModel()).' not found',
+            ], 404);
+        }
+
+        return parent::render($request, $exception);
+    }
+
     /**
      * Register the exception handling callbacks for the application.
      *
@@ -36,6 +48,39 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (InvalidFieldException $e, $request) {
+            $fields = config('subscriber.fields.allowed');
+            $last = array_pop($fields);
+
+            return response()->json([
+                'status' => 'ERROR',
+                'error' => 'Invalid field passed',
+                'help' => 'Fields must have a value and a type of either '.implode(', ', $fields)." or {$last}",
+            ], 404);
+        });
+
+        $this->renderable(function (InvalidStateException $e, $request) {
+            $states = State::all()->pluck('name')->toArray();
+            $last = array_pop($states);
+
+            return response([
+                'status' => 'ERROR',
+                'error' => 'Invalid State used',
+                'help' => 'You can only use '.implode(", ", $states)." or {$last}",
+            ], 404);
+        });
+
+        $this->renderable(function (InvalidSearchTermException $e, $request) {
+            $searchTerms = config('subscriber.search_terms.allowed');
+            $last = array_pop($searchTerms);
+
+            return response()->json([
+                'status' => 'ERROR',
+                'error' => 'Invalid search term',
+                'help' => 'You can only search by '.implode(', ', $searchTerms)." or {$last}",
+            ], 404);
         });
     }
 }
---
+++ app/Exceptions/InvalidFieldException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class InvalidFieldException extends Exception
+{
+    public function render($request)
+    {
+
+    }
+}
